d8c90daa262e0e56af2f6f4a088d6c990cca7f7e
jmoncayo-pursuit <jmoncayo@pursuit.org>
2026-05-06 17:13:42 -0400

    chore: add Dockerfile and prepare for Google Cloud Run deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: import('next').NextConfig = {
+  output: 'standalone',
+  // other configs if you had any
 };
 
 export default nextConfig;
